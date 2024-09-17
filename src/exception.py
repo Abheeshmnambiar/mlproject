068c186b226aec1d45726c98671606a9f61a209f
Abheesh M <abheeshmnambiar@gmail.com>
2024-09-17 16:22:56 +0530

    notbooks and data_ingestion.py

--- src/exception.py
+++ src/exception.py
@@ -20,5 +20,10 @@ class CustomException(Exception):
         return self.error_message
     
 
-
+# if __name__=="__main__":
+#     try :
+#         a= 23/0
+#     except Exception as e:
+#         logging.info("Divided by zero")
+#         raise CustomException(e,sys)
         
